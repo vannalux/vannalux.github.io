6f85965f3e2b9eeb25b03623a52a8c0266db91f4
vannalux <remtexvanna@gmail.com>
2017-08-25 12:30:39 +0300

    f

---
+++ res/blog.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+$imSettings['blog'] = array(
+	'description' => 'Когда Вам нужно выбрать и купить ламинат в Одессе, то стоит обратить пристальное внимание на его характеристики. То же самое можно сказать и про паркет. Одесса - один из тех городов, где есть большой выбор продукции самого разного качества. И чтобы купить паркет в Одессе, который будет Вам идеально подходить, стоит учесть некоторые ключевые моменты.',
+	'root' => 'http://moy.cc.ua/blog/',
+	'file_prefix' => 'x5_',
+	'comments_source' => 'wsx5',
+	'email' => '',
+	'moderate' => false,
+	'sendmode' => 'file',
+	'folder' => '',
+	'comment_type' => 'both',
+	'comments_order' => 'desc',
+	'abuse' => false,
+	'captcha' => false,
+	'post_type' => 'firstshown',
+	'categories' => Array('22'),
+	'posts' => Array(),
+	'posts_cat' => Array(),
+	'posts_month' => Array()
+);
+
+// Post titled "ЛАМИНАТ"
+$imSettings['blog']['posts']['78g2ptmf'] = array(
+	'id' => '78g2ptmf',
+	'title' => 'ЛАМИНАТ',
+	'author' => '',
+	'category' => '22',
+	'summary' => '',
+	'body' => "<div id=\"imBlogPost_78g2ptmf\">Когда Вам нужно выбрать и купить ламинат в Одессе, то стоит обратить пристальное внимание на его характеристики. То же самое можно сказать и про паркет. Одесса - один из тех городов, где есть большой выбор продукции самого разного качества. И чтобы купить паркет в Одессе, который будет Вам идеально подходить, стоит учесть некоторые ключевые моменты.<div style=\"clear: both;\"><!-- clear floated images --></div></div>",
+	'timestamp' => '25/8/2017 12:07:00',
+	'utc_time' => 1503652020,
+	'month' => '201708',
+	'comments' => false,
+	'sources' => array(),
+	'tag' => array(),
+	'opengraph' => array(
+		'url' => 'http://moy.cc.ua/blog/?id=78g2ptmf',
+		'type' => 'article',
+		'title' => 'ЛАМИНАТ',
+		'description' => '',
+		'updated_time' => '1503655620',
+		'images' => array()
+	)
+);$imSettings['blog']['posts_cat']['22'][] = '78g2ptmf';
+$imSettings['blog']['posts_month']['201708'][] = '78g2ptmf';
+
+// End of file blog.inc.php
