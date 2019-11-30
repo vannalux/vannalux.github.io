cbee2f2ab2b1637f385d6af30458a54e6750ce1d
vannalux <remtexvanna@gmail.com>
2019-11-30 22:06:26 +0200

    m55

---
+++ res/search.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+/*****************
+	GENERAL SETTINGS
+*****************/
+$imSettings['search']['general'] = array(
+	'menu_position' => 'left',
+	'defaultScope' => array(
+		'0' => 'index.html',
+		'6' => 'cena.html',
+		'7' => '---------14.html',
+		'8' => '---------15.html',
+		'9' => '---------16.html',
+		'10' => '---------17.html',
+		'11' => '---------18.html',
+		'12' => '---------19.html'
+	),
+	'extendedScope' => array(
+	)
+);
+
+/*****************
+	PRODUCTS SEARCH
+*****************/
+$imSettings['search']['products'] = array(
+);
+
+/*****************
+	IMAGES SEARCH
+*****************/
+$imSettings['search']['images'] = array(
+	array(
+		'title' => '',
+		'description' => '',
+		'location' => '',
+		'src' => 'images/765432122w.png',
+		'page' => 'index.html'
+	)
+);
+
+/*****************
+	VIDEOS SEARCH
+*****************/
+$imSettings['search']['videos'] = array(
+);
+$imSettings['search']['dynamicobjects'] = array(
+
+);
+
+// End of file search.inc.php
