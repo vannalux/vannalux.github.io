b6a6d3633a3b13ae69df811c4b19f05449d01d2f
vannalux <remtexvanna@gmail.com>
2019-12-02 15:03:46 +0200

    55

---
+++ imsearch.php
@@ -0,0 +1,187 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="ru-RU" dir="ltr">
+	<head>
+		<title>Поиск - Vannalux</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="Vannalux" />
+		<meta name="generator" content="Incomedia WebSite X5 Evo 2019.3.9 - www.websitex5.com" />
+		<meta property="og:image" content="https://vannalux.github.io/favImage.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="600">
+		<meta property="og:image:height" content="400">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" href="style/reset.css?2019-3-9-1" media="screen,print" />
+		<link rel="stylesheet" href="style/print.css?2019-3-9-1" media="print" />
+		<link rel="stylesheet" href="style/style.css?2019-3-9-1" media="screen,print" />
+		<link rel="stylesheet" href="style/template.css?2019-3-9-1" media="screen" />
+		<link rel="stylesheet" href="pcss/imsearch.css?2019-3-9-1-637108951378672763" media="screen,print" />
+		<script src="res/jquery.js?2019-3-9-1"></script>
+		<script src="res/x5engine.js?2019-3-9-1" data-files-version="2019-3-9-1"></script>
+		<script>
+		x5engine.boot.push(function(){ x5engine.contentSlider.init( { classes: { root: 'content-slider-root', slide: 'slide-container', animated: 'slide-animated-element', ctrlContainer: 'content-slider-ctrl-container', ctrlVisible: 'ctrl-visible', ctrlVisibleOnMobile: 'ctrl-visible-on-mobile', prev: '-prev', next: '-next', defaultSlide: 'default-slide', slideOut: 'exiting-slide', slideIn: 'entering-slide', noElementEffect: 'element-effect-none', radioIdSeparator: '-slide-radio-', radioBar: 'current-slide-radio-container', radioBarNext: 'radio-bar-next', radioBarPrev: 'radio-bar-prev' }, radio: { visibleNumber: 15, size: 22, slideBarGap: 10 }, config: { radioSliding: { enabled: false }, ctrlHidingDuringAnimations: { enabled: true, animations: [ 'change-effect-rotateX', 'change-effect-rotateY', 'change-effect-cube' ] } } } ); }, false, 7);
+		</script>
+		<script>
+			window.onload = function(){ checkBrowserCompatibility('Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','Возможно, Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','[1]Обновите Ваш браузер[/1] или [2]продолжите процедуру [/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		<link rel="icon" href="favicon.png?2019-3-9-1-637108951377703109" type="image/png" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<header id="imHeader">
+						<h1 class="imHidden">Поиск - Vannalux</h1>
+						<div id="imHeaderObjects"><div id="imHeader_imMenuObject_12_wrapper" class="template-object-wrapper"><!-- UNSEARCHABLE --><div id="imHeader_imMenuObject_12"><div id="imHeader_imMenuObject_12_container"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden">
+	<li class="imMnMnFirst imPage" data-link-paths=",/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Главная		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/cena.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="cena.html">
+Цены		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/metodi-restavracii.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="metodi-restavracii.html">
+Методы реставрации		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/foto.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="foto.html">
+Фото		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imLevel" data-link-paths=",/index.html,/" data-link-anchor="kontakty" data-link-hash="1338006685"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#kontakty" class="label" onclick="return x5engine.utils.location('index.html#kontakty', null, false)">Контакты</a></div></div></li></ul></div></div><!-- UNSEARCHABLE END --><script>
+var imHeader_imMenuObject_12_settings = {
+	'menuId': 'imHeader_imMenuObject_12',
+	'responsiveMenuEffect': 'slide',
+	'animationDuration': 1000,
+}
+x5engine.boot.push(function(){x5engine.initMenu(imHeader_imMenuObject_12_settings)});
+$(function () {$('#imHeader_imMenuObject_12_container ul li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imHeader_imMenuObject_12_container-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imHeader_imMenuObject_12_container-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div><div id="imHeader_imObjectTitle_14_wrapper" class="template-object-wrapper"><div id="imHeader_imObjectTitle_14"><span id ="imHeader_imObjectTitle_14_text" >Реставрация Ванн</span></div></div><div id="imHeader_imObjectTitle_15_wrapper" class="template-object-wrapper"><div id="imHeader_imObjectTitle_15"><span id ="imHeader_imObjectTitle_15_text" >VannaLux</span></div></div><div id="imHeader_imObjectTitle_16_wrapper" class="template-object-wrapper"><div id="imHeader_imObjectTitle_16"><span id ="imHeader_imObjectTitle_16_text" >Реставрация Ванн</span></div></div><div id="imHeader_imObjectContentSlider_17_wrapper" class="template-object-wrapper"><div id="imHeader_imObjectContentSlider_17" class="content-slider content-slider-root"><div class="content-slider content-slider-container change-effect-none"><div id="imHeader_imObjectContentSlider_17-slide-0" class="content-slider slide-container default-slide"><div class="content-slider slide-grid-container only-txt-grid image-overlap element-effect-none"><div class="content-slider slide-element slide-txt-container"><div class="content-slider slide-txt-content"><div class="content-slider slide-animated-element slide-txt-block-container slide-description"><div class="content-slider slide-txt-block-content slide-animated-content slide-description" >Выбери<br />Реставрацию Ванны<br />от профессионалов<br /><br />(097) 527-02-80  Киевстар<br />(095) 106-00-43  Vodafone </div></div></div></div></div></div></div></div></div></div>
+					</header>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imMenuObject_01_wrapper" class="template-object-wrapper"><!-- UNSEARCHABLE --><div id="imStickyBar_imMenuObject_01"><div id="imStickyBar_imMenuObject_01_container"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden">
+	<li class="imMnMnFirst imPage" data-link-paths=",/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Главная		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/cena.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="cena.html">
+Цены		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/metodi-restavracii.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="metodi-restavracii.html">
+Методы реставрации		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/foto.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="foto.html">
+Фото		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imLevel" data-link-paths=",/index.html,/" data-link-anchor="kontakty" data-link-hash="1338006685"><div class="label-wrapper"><div class="label-inner-wrapper"><a href="index.html#kontakty" class="label" onclick="return x5engine.utils.location('index.html#kontakty', null, false)">Контакты</a></div></div></li></ul></div></div><!-- UNSEARCHABLE END --><script>
+var imStickyBar_imMenuObject_01_settings = {
+	'menuId': 'imStickyBar_imMenuObject_01',
+	'responsiveMenuEffect': 'slide',
+	'animationDuration': 1000,
+}
+x5engine.boot.push(function(){x5engine.initMenu(imStickyBar_imMenuObject_01_settings)});
+$(function () {$('#imStickyBar_imMenuObject_01_container ul li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imStickyBar_imMenuObject_01_container-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imStickyBar_imMenuObject_01_container-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Заголовок главного меню">Перейти к контенту</a>
+					<div id="imSideBar">
+						<div id="imSideBarObjects"></div>
+					</div>
+					<div id="imContentGraphics"></div>
+					<main id="imContent">
+						<a id="imGoToCont"></a>
+						<div id="imSearchPage">
+						<h2 id="imPgTitle">Результаты поиска</h2>
+						<?php
+						$search = new imSearch();
+						$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+						$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+						$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+						<div class="searchPageContainer">
+						<?php echo $search->search($keys, $page, $type); ?>
+						</div>
+						</div>
+						
+					</main>
+					<footer id="imFooter">
+						<div id="imFooterObjects"><div id="imFooter_imTextObject_01_wrapper" class="template-object-wrapper"><div id="imFooter_imTextObject_01">
+	<div data-index="0"  class="text-tab-content grid-prop current-tab "  id="imFooter_imTextObject_01_tab0" style="opacity: 1; ">
+		<div class="text-inner">
+			<div class="imTACenter"><i><span class="fs10lh1-5 cf1">Реставрируем ванны в городах Украины таких как:</span></i></div><div class="imTACenter"><i><span class="fs10lh1-5 cf1">Киев, Одесса, Харьков, Днепропетровск, Запорожье, Николаев, Херсон, Львов, Хмельницкий, Житомир, Винница, Полтава, Черкассы, Кировоград, Луцк, Кривой Рог, Ровно...</span></i></div><div class="imTACenter"><i><br></i></div><div class="imTACenter"><i><span class="fs10lh1-5 cf1">Реставрация ванн, эмалировка ванн, наливная ванна, эмалировка ванн акрилом © VANNALUX.ML Украина 2010-2019</span></i></div>
+		</div>
+	</div>
+
+</div>
+</div><div id="imFooter_imTextObject_02_wrapper" class="template-object-wrapper"><div id="imFooter_imTextObject_02">
+	<div data-index="0"  class="text-tab-content grid-prop current-tab "  id="imFooter_imTextObject_02_tab0" style="opacity: 1; ">
+		<div class="text-inner">
+			<div class="imTACenter"><i><span class="fs8lh1-5 cf1">Реставрируем ванны в городах Украины таких как:</span></i></div><div class="imTACenter"><i><span class="fs8lh1-5 cf1">Киев, Одесса, Харьков, Днепропетровск, Запорожье, Николаев, Херсон, Львов, Хмельницкий, Житомир, Винница, Полтава, Черкассы, Кировоград, Луцк, Кривой Рог, Ровно...</span></i></div><div class="imTACenter"><i class="fs8lh1-5"><br></i></div><div class="imTACenter"><i><span class="fs8lh1-5 cf1">Реставрация ванн, эмалировка ванн, наливная ванна, эмалировка ванн акрилом © VANNALUX.ML Украина 2010-2019</span></i></div>
+		</div>
+	</div>
+
+</div>
+</div></div>
+					</footer>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Прочесть эту страницу заново">Назад к содержимому</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Для использования этого сайта необходимо включить JavaScript.</div></noscript>
+	</body>
+</html>
