f23a4b3a370e3721d0fe989b90f389db8be6325d
vannalux <remtexvanna@gmail.com>
2020-02-17 23:12:37 +0200

    55

---
+++ res/access.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+// Users data
+$imSettings['access']['users'] = array(
+	'vannavam2015@gmail.com' => array(
+		'groups' => array('dpb38v6c'),
+		'id' => 'yobwep9b',
+		'firstname' => 'Admin',
+		'lastname' => 'Admin',
+		'password' => 'O$0o!<W4R4mk',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('yobwep9b');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
