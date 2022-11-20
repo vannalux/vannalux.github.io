abb34179fc4d79745904caba2ed7b211c0ab256e
vannalux <remtexvanna@gmail.com>
2022-11-20 14:42:22 +0200

    55

---
+++ imsearch.php
@@ -0,0 +1,173 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="ru-RU" dir="ltr">
+	<head>
+		<title>Поиск</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="NAIL-GURU.TK" />
+		<meta property="og:locale" content="ru" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="https://nail-guru.tk/imsearch.php" />
+		<meta property="og:title" content="Поиск" />
+		<meta property="og:site_name" content="Маникюр и педикюр в Одессе" />
+		<meta property="og:image" content="https://nail-guru.tk/favImage.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="800">
+		<meta property="og:image:height" content="800">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" href="style/reset.css?2022-3-4-0" media="screen,print" />
+		<link rel="stylesheet" href="style/print.css?2022-3-4-0" media="print" />
+		<link rel="stylesheet" href="style/style.css?2022-3-4-0" media="screen,print" />
+		<link rel="stylesheet" href="style/template.css?2022-3-4-0" media="screen" />
+		
+		<link rel="stylesheet" href="pluginAppObj/imFooter_pluginAppObj_05/custom.css" media="screen, print" />
+		<link rel="stylesheet" href="pcss/imsearch.css?2022-3-4-0-638045521074634318" media="screen,print" />
+		<script src="res/jquery.js?2022-3-4-0"></script>
+		<script src="res/x5engine.js?2022-3-4-0" data-files-version="2022-3-4-0"></script>
+		
+		<script src="pluginAppObj/imFooter_pluginAppObj_05/main.js"></script>
+		<script>
+			window.onload = function(){ checkBrowserCompatibility('Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','Возможно, Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','[1]Обновите Ваш браузер[/1] или [2]продолжите процедуру [/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		<link rel="icon" href="favicon.png?2022-3-4-0-638045521074634318" type="image/png" />
+		<style>
+				h1, h2, h3, h4, h5, h6 {
+				    font-weight: normal;
+				}
+		</style>
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<header id="imHeader">
+						
+						<div id="imHeaderObjects"><div id="imHeader_imObjectTitle_03_wrapper" class="template-object-wrapper"><div id="imHeader_imObjectTitle_03"><span id ="imHeader_imObjectTitle_03_text" ><a href="index.html" onclick="return x5engine.utils.location('index.html', null, false)">NAIL GURU</a></span></div></div><div id="imHeader_imMenuObject_01_wrapper" class="template-object-wrapper"><!-- UNSEARCHABLE --><div id="imHeader_imMenuObject_01"><div id="imHeader_imMenuObject_01_container"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden">
+	<li class="imMnMnFirst imPage" data-link-paths=",/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Главная		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/price.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="price.html">
+Прайс		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths=",/foto.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="foto.html">
+Фото		</a>
+</div>
+</div>
+	</li></ul></div></div><!-- UNSEARCHABLE END --><script>
+var imHeader_imMenuObject_01_settings = {
+	'menuId': 'imHeader_imMenuObject_01',
+	'responsiveMenuEffect': 'slide',
+	'responsiveMenuLevelOpenEvent': 'mouseover',
+	'animationDuration': 600,
+}
+x5engine.boot.push(function(){x5engine.initMenu(imHeader_imMenuObject_01_settings)});
+$(function () {$('#imHeader_imMenuObject_01_container ul li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).show(); }, 250);    }).on('mouseleave', function () {        timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).hide(); }, 250);    });});});
+
+</script>
+</div><div id="imHeader_imTextObject_04_wrapper" class="template-object-wrapper"><div id="imHeader_imTextObject_04">
+	<div data-index="0"  class="text-tab-content grid-prop current-tab "  id="imHeader_imTextObject_04_tab0" style="opacity: 1; ">
+		<div class="text-inner">
+			<div class="imTACenter"><b><a href="tel:+380661861072" class="imCssLink"><span class="fs16lh1-5 cf1 ff1">(066) 186-10-72 &nbsp;Vodafone</span></a></b></div>
+		</div>
+	</div>
+
+</div>
+</div></div>
+					</header>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imTextObject_05_wrapper" class="template-object-wrapper"><div id="imStickyBar_imTextObject_05">
+	<div data-index="0"  class="text-tab-content grid-prop current-tab "  id="imStickyBar_imTextObject_05_tab0" style="opacity: 1; ">
+		<div class="text-inner">
+			<div class="imTACenter"><a href="tel:+380661861072" class="imCssLink"><span class="fs16lh1-5 cf1 ff1"><b>(066) 186-10-72 &nbsp;Vodafone</b></span></a><span class="fs16lh1-5 cf2 ff1"> &nbsp;Пн-Пт с 9:00-19:00</span></div>
+		</div>
+	</div>
+
+</div>
+</div><div id="imStickyBar_imTextObject_06_wrapper" class="template-object-wrapper"><div id="imStickyBar_imTextObject_06">
+	<div data-index="0"  class="text-tab-content grid-prop current-tab "  id="imStickyBar_imTextObject_06_tab0" style="opacity: 1; ">
+		<div class="text-inner">
+			<div class="imTACenter"><span class="fs16lh1-5 ff1"><a href="tel:+380661861072" class="imCssLink"><b><span class="cf1">tel: (066) 186-10-72 &nbsp;Vodafone</span></b></a></span></div>
+		</div>
+	</div>
+
+</div>
+</div><div id="imStickyBar_imObjectImage_07_wrapper" class="template-object-wrapper"><div id="imStickyBar_imObjectImage_07"><div id="imStickyBar_imObjectImage_07_container"><a href="viber://chat?number=380661861072"><img src="images/viber_PNG10_bcfjanpw.webp" title="Viber" alt="Viber" width="32" height="34" />
+</a></div></div></div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Заголовок главного меню">Перейти к контенту</a>
+					<div id="imSideBar">
+						<div id="imSideBarObjects"><div id="imSideBar_imObjectImage_01_wrapper" class="template-object-wrapper"><div id="imSideBar_imObjectImage_01"><div id="imSideBar_imObjectImage_01_container"><img src="images/empty-GT_imagea-1-.webp" title="" alt="" width="140" height="140" />
+</div></div></div></div>
+					</div>
+					<div id="imContentGraphics"></div>
+					<main id="imContent">
+						<a id="imGoToCont"></a>
+						<div id="imSearchPage">
+						<h1 id="imPgTitle">Результаты поиска</h1>
+						<?php
+						$search = new imSearch();
+						$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search'], ENT_COMPAT) : "";
+						$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+						$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+						<div class="searchPageContainer">
+						<?php echo $search->search($keys, $page, $type); ?>
+						</div>
+						</div>
+						
+					</main>
+					<footer id="imFooter">
+						<div id="imFooterObjects"><div id="imFooter_imTextObject_02_wrapper" class="template-object-wrapper"><div id="imFooter_imTextObject_02">
+	<div data-index="0"  class="text-tab-content grid-prop current-tab "  id="imFooter_imTextObject_02_tab0" style="opacity: 1; ">
+		<div class="text-inner">
+			<div class="imTACenter"><i><span class="fs16lh1-5 ff1"><a href="tel:+380661861072" class="imCssLink"><b><span class="cf1">(066) 186-10-72 &nbsp;Vodafone</span></b></a></span><span class="fs16lh1-5 cf2 ff1"> Пн-Пт с 9:00-19:00</span></i></div><div class="imTACenter"><i class="fs10lh1-5"><span class="cf2">© </span><a href="index.html" class="imCssLink" onclick="return x5engine.utils.location('index.html', null, false)"><span class="cf2">NAIL-GURU.TK</span></a><span class="cf2"> Украина 2015-<script type="text/javascript">document.write(new Date().getFullYear());</script></span></i></div>
+		</div>
+	</div>
+
+</div>
+</div><div id="imFooter_pluginAppObj_05_wrapper" class="template-object-wrapper"><!-- Social Icons v.16 --><div id="imFooter_pluginAppObj_05">
+            <div id="soc_imFooter_pluginAppObj_05">
+                <div class="wrapper horizontal original none">
+                	<div class='social-icon original'><a href='#' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><g><rect width="100" height="100" style="fill:#3d5a98"/><g><path d="M69,100V61.27H82l1.95-15.09H69V36.55c0-4.37,1.21-7.35,7.48-7.35h8V15.7a106.94,106.94,0,0,0-11.65-.59c-11.52,0-19.41,7-19.41,19.95V46.18h-13V61.27h13V100H69Z" style="fill:#fff"/></g></g></svg><span class='fallbacktext'>Fb</span></a></div><div class='social-icon original'><a href='#' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100"><defs><radialGradient id="shade_ig_imFooter_pluginAppObj_05" cx="18.44" cy="364.72" r="115" gradientTransform="matrix(1, 0, 0, -1, 0, 460.03)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ffb140"/><stop offset="0.26" stop-color="#ff5445"/><stop offset="0.6" stop-color="#fc2b82"/><stop offset="1" stop-color="#8e40b7"/></radialGradient></defs><g><g><rect x="5" y="5" width="90" height="90" style="fill:none"/><path d="M79.42,26a5.4,5.4,0,1,1-5.4-5.4A5.4,5.4,0,0,1,79.42,26ZM50,65A15,15,0,1,1,65,50,15,15,0,0,1,50,65Zm0-38.11A23.11,23.11,0,1,0,73.11,50,23.11,23.11,0,0,0,50,26.89Zm0-13.78c12,0,13.44,0,18.18.26a24.89,24.89,0,0,1,8.36,1.55,13.94,13.94,0,0,1,5.17,3.37,13.93,13.93,0,0,1,3.37,5.17,24.91,24.91,0,0,1,1.55,8.36c0.22,4.75.26,6.17,0.26,18.18s0,13.44-.26,18.18a24.89,24.89,0,0,1-1.55,8.36,14.91,14.91,0,0,1-8.54,8.54,24.91,24.91,0,0,1-8.36,1.55C63.44,86.85,62,86.89,50,86.89s-13.44,0-18.18-.26a24.89,24.89,0,0,1-8.36-1.55,13.94,13.94,0,0,1-5.17-3.37,13.94,13.94,0,0,1-3.37-5.17,24.91,24.91,0,0,1-1.55-8.36C13.15,63.44,13.11,62,13.11,50s0-13.44.26-18.18a24.89,24.89,0,0,1,1.55-8.36,13.94,13.94,0,0,1,3.37-5.17,13.93,13.93,0,0,1,5.17-3.37,24.91,24.91,0,0,1,8.36-1.55C36.56,13.15,38,13.11,50,13.11ZM50,5c-12.22,0-13.75.05-18.55,0.27A33,33,0,0,0,20.52,7.36a22.05,22.05,0,0,0-8,5.19,22.05,22.05,0,0,0-5.19,8A33,33,0,0,0,5.27,31.45C5.05,36.25,5,37.78,5,50s0.05,13.75.27,18.55A33,33,0,0,0,7.36,79.48a22.05,22.05,0,0,0,5.19,8,22.06,22.06,0,0,0,8,5.19,33,33,0,0,0,10.92,2.09C36.25,94.95,37.78,95,50,95s13.75-.05,18.55-0.27a33,33,0,0,0,10.92-2.09A23,23,0,0,0,92.64,79.48a33,33,0,0,0,2.09-10.92C94.95,63.75,95,62.22,95,50s-0.05-13.75-.27-18.55a33,33,0,0,0-2.09-10.92,22.06,22.06,0,0,0-5.19-8,22.06,22.06,0,0,0-8-5.19A33,33,0,0,0,68.55,5.27C63.75,5.05,62.22,5,50,5Z" style="fill:url(#shade_ig_imFooter_pluginAppObj_05)"/></g></g></svg><span class='fallbacktext'>Ig</span></a></div>
+                </div>
+
+            </div>
+                <script>
+                    socialicons_imFooter_pluginAppObj_05();
+                </script>
+        </div></div><div id="imFooter_imObjectImage_06_wrapper" class="template-object-wrapper"><div id="imFooter_imObjectImage_06"><div id="imFooter_imObjectImage_06_container"><img src="images/telega.webp" title="Телеграм" alt="Телеграм" width="33" height="33" />
+</div></div></div><div id="imFooter_imObjectImage_07_wrapper" class="template-object-wrapper"><div id="imFooter_imObjectImage_07"><div id="imFooter_imObjectImage_07_container"><a href="viber://chat?number=380661861072"><img src="images/viber_PNG10.webp" title="Viber" alt="Viber" width="33" height="35" />
+</a></div></div></div></div>
+					</footer>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Прочесть эту страницу заново">Назад к содержимому</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Для использования этого сайта необходимо включить JavaScript.</div></noscript>
+	</body>
+</html>
