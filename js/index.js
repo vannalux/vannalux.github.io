3514cab49d73c6a366f90865d2f07975e3d1400a
vannalux <31038037+vannalux@users.noreply.github.com>
2024-01-31 21:26:03 +0200

    555

---
+++ js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(event)
+{
+   var Carousel1 = new bootstrap.Carousel('#Carousel1', {interval: 4000, pause: false});
+});
+$(document).ready(function()
+{
+   $('#wb_LayoutGrid1').parallax();
+   $("a[href*='#LayoutGrid4']").click(function(event)
+   {
+      event.preventDefault();
+      $('html, body').stop().animate({ scrollTop: $('#wb_LayoutGrid4').offset().top-88 }, 600, 'easeOutCirc');
+   });
+   $("a[href*='#LayoutGrid3']").click(function(event)
+   {
+      event.preventDefault();
+      $('html, body').stop().animate({ scrollTop: $('#wb_LayoutGrid3').offset().top-88 }, 600, 'easeOutCirc');
+   });
+});
